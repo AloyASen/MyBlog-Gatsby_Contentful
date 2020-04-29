e2b9ae1a4cb254a3a125b20ffc686a1bbb23954d
Aditya Sen <aloysen951@gmail.com>
2020-04-29 20:45:28 +0530

    just some adjustments to the home page to give a refresher about contacting me

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ const IndexPage = () => {
             <Head title="Home" />
             <h1>Hello.</h1>
             <h2>I'm Adiya, CS-major with experience in web development, IoT Systems Enginnering and Data Science Research. </h2>
+            <p>Need a Developer <Link to="/contact">Contact Me!</Link></p>
         </Layout>
     )
 }
