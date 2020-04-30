9aeaae323c0bb0e419250a7864e54135f28f3879
Aditya Sen <aloysen951@gmail.com>
2020-04-30 14:17:12 +0530

    some changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
         'gatsby-plugin-react-helmet',
         {
             resolve: 'gatsby-source-contentful',
+            downloadLocal: true,
             options: {
                 spaceId: process.env.CONTENTFUL_SPACE_ID,
                 accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -21,9 +21,10 @@ const Blog = props => {
   const options = {
     renderNode: {
       "embedded-asset-block": (node) => {
-        const alt = node.data.target.fields.title['en-US']
-        const url = node.data.target.fields.file['en-US'].url
-        return <img alt={alt} src={url} />
+        // const alt = node.data.target.fields.title['en-US']
+        // const url = node.data.target.fields.file['en-US'].url
+        // return <img alt={alt} src={url} />
+        console.log(node);
       }
     }
   }
