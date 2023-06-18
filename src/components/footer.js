14c99575faf48045081f28f8a57bc7bc40729530
Aloy Aditya Sen <aloysen951@gmail.com>
2023-06-18 14:04:07 +0530

    modifying the Footer at voice o sen

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,9 +6,8 @@ import * as styles from './footer.module.css'
 const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
-      Built with <a href="https://contentful.com/">Contentful</a> and{' '}
-      <a href="https://gatsbyjs.com">Gatsby</a> &middot;{' '}
-      <a href="https://github.com/contentful/starter-gatsby-blog">Source</a>
+      Invite <a href="www.linkedin.com/in/aloyadityasen">me to Coffee</a> and{' '}
+      <a href="https://github.com/contentful/starter-gatsby-blog">Lets' Meetup</a>
     </div>
   </Container>
 )
